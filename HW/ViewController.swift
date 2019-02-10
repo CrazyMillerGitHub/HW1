576a81fb179a90f57ba09e5ce73637f5bf7178c4
dsgnmike <mishaborisov2013@gmail.com>
2019-02-10 19:34:03 +0300

    -Added Function

--- HW/ViewController.swift
+++ HW/ViewController.swift
@@ -15,23 +15,29 @@ class ViewController: UIViewController {
     // Do any additional setup after loading the view, typically from a nib.
   }
   override func viewWillAppear(_ animated: Bool) {
-    print("App will be appear")
+    print("Application moved from DidLoad to Appearing: \(#function)")
   }
-//  override func viewDidAppear(_ animated: Bool) {
-//    <#code#>
-//  }
-//  override func viewDidLayoutSubviews() {
-//    <#code#>
-//  }
-//  override func viewWillLayoutSubviews() {
-//    <#code#>
-//  }
-//  override func viewWillDisappear(_ animated: Bool) {
-//    <#code#>
-//  }
-//  override func viewDidDisappear(_ animated: Bool) {
-//    <#code#>
-//  }
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(true)
+   print("Application moved from DidLayoutSubviews to Appeared: \(#function)")
+  }
+  override func viewWillLayoutSubviews() {
+    super.viewWillLayoutSubviews()
+    print("Application moved from Appearing to WillLayoutSubviews: \(#function)")
+  }
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    print("Application moved from WillLayoutSubviews to DidLayoutSubviews: \(#function)")
+  }
+
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(true)
+    print("5")
+  }
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(true)
+    print("6")
+  }
 }
 
 
