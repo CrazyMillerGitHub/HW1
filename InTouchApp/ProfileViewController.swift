e873022703b00ce8149905061cade2851d0fdee3
dsgnmike <mishaborisov2013@gmail.com>
2019-02-16 17:34:03 +0300

    -Added new functions

--- InTouchApp/ProfileViewController.swift
+++ InTouchApp/ProfileViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ProfileViewController.swift
 //  InTouchApp
 //
 //  Created by Михаил Борисов on 08/02/2019.
@@ -8,16 +8,18 @@
 
 import UIKit
 
-@IBDesignable
-class ViewController: UIViewController {
+
+@IBDesignable class ProfileViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
   
   @IBOutlet weak var addImageButton: UIButton!
   @IBOutlet weak var imageView: UIImageView!
   @IBOutlet weak var editButton: UIButton!
+  let imagePicker = UIImagePickerController()
   override func viewDidLoad() {
     super.viewDidLoad()
     
     // Do any additional setup after loading the view, typically from a nib.
+    
     editButton.layer.borderWidth = 1
     editButton.layer.borderColor = UIColor.black.cgColor
     editButton.layer.cornerRadius = 15
@@ -25,6 +27,7 @@ class ViewController: UIViewController {
     imageView.layer.cornerRadius = 30
     imageView.layer.masksToBounds = true
     Logging().check("Is logging = \(StruckOfLog.isLogging)")
+    addImageButton.addTarget(self, action: #selector(addImageButtonAction), for: .touchUpInside)
   }
   override func viewWillAppear(_ animated: Bool) {
     Logging().check("Application moved from DidLoad to Appearing: \(#function)")
@@ -50,6 +53,29 @@ class ViewController: UIViewController {
     super.viewDidDisappear(true)
     Logging().check("6")
   }
+  @objc func addImageButtonAction(){
+    let myPickerController = UIImagePickerController()
+    myPickerController.delegate = self
+    let alertController = UIAlertController(title: "Add image", message: nil, preferredStyle: .actionSheet)
+    let action1 = UIAlertAction(title: "Take a picture", style: .default) { (action:UIAlertAction) in myPickerController.sourceType = .camera
+        self.present(myPickerController, animated: true, completion: nil)
+
+       }
+    
+    let action2 = UIAlertAction(title: "Choose from library", style: .default) { (action:UIAlertAction) in
+      myPickerController.sourceType = .photoLibrary
+      self.present(myPickerController, animated: true, completion: nil)
+    }
+    alertController.addAction(action1)
+    alertController.addAction(action2)
+    self.present(alertController, animated: true, completion: nil)
+  }
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    if let image = info[.originalImage] as? UIImage {
+      self.imageView.image = image
+    }
+    self.dismiss(animated: true, completion: nil)
+  }
+
+  
 }
-
-
