feb359ab41b2d3bf0279ab7512bfe8044410abe7
dsgnmike <mishaborisov2013@gmail.com>
2019-02-24 19:55:26 +0300

    -Spaces added

--- InTouchApp/ConversationViewController.swift
+++ InTouchApp/ConversationViewController.swift
@@ -14,13 +14,14 @@ protocol MessageCellConfiguration:class {
 class ConversationViewController: UIViewController {
   var arr = ["Привет, серъезный вопрос...", "Привет, какой?", "Идём?", "Куда?", "Ты знаешь...", "Аааа, ок"]
   @IBOutlet weak var tableView: UITableView!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     navigationController?.navigationBar.prefersLargeTitles = false
     extendedLayoutIncludesOpaqueBars = true
     tableView.delegate = self
     tableView.dataSource = self
-        // Do any additional setup after loading the view.
     }
 }
 extension ConversationViewController: UITableViewDelegate, UITableViewDataSource {
@@ -28,19 +29,20 @@ extension ConversationViewController: UITableViewDelegate, UITableViewDataSource
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return arr.count
   }
-  
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     if indexPath.row % 2 == 0
     {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath) as! CustomConversationCell1
+      let cell = tableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath) as! CustomConversationCell1
       cell.config(text: arr[indexPath.row])
       return cell
     }
-    let cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! CustomConversationCell2
-    cell.config(text: arr[indexPath.row])
-    return cell
+      let cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! CustomConversationCell2
+      cell.config(text: arr[indexPath.row])
+      return cell
   }
 }
+
+
 class CustomConversationCell1: UITableViewCell,MessageCellConfiguration{
   var txt: String?
   @IBOutlet weak var bgImage: UIImageView!
@@ -52,15 +54,15 @@ class CustomConversationCell1: UITableViewCell,MessageCellConfiguration{
     bgImage.image = UIImage(named: "bubble_gray")!.resizableImage(withCapInsets: UIEdgeInsets(top: 22, left: 26, bottom: 22, right: 26)).withRenderingMode(.alwaysTemplate)
   }
 }
+
+
 class CustomConversationCell2: UITableViewCell,MessageCellConfiguration{
   var txt: String?
   @IBOutlet weak var bgImage: UIImageView!
   @IBOutlet weak var outTextLabel: UILabel!
   func config(text: String){
-    
     self.txt = text
     outTextLabel.text = self.txt
     bgImage.image = UIImage(named: "bubble_blue")!.resizableImage(withCapInsets: UIEdgeInsets(top: 22, left: 26, bottom: 22, right: 26)).withRenderingMode(.alwaysTemplate)
- 
   }
 }
