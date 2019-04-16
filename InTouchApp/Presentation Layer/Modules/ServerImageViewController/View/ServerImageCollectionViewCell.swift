5e9cf9836138e7467c924a2e0ea14ea2fe861624
dsgnmike <mishaborisov2013@gmail.com>
2019-04-16 21:45:56 +0300

    -few change

--- InTouchApp/Presentation Layer/Modules/ServerImageViewController/View/ServerImageCollectionViewCell.swift
+++ InTouchApp/Presentation Layer/Modules/ServerImageViewController/View/ServerImageCollectionViewCell.swift
@@ -1,8 +1,9 @@
 //
 //  ServerImageCollectionViewCell.swift
-//  
+//  InTouchApp
 //
 //  Created by Михаил Борисов on 16/04/2019.
+//  Copyright © 2019 Mikhail Borisov. All rights reserved.
 //
 
 import UIKit
